1f295db526186ebf1b27f0f3097d242bc113f6f0
rajoyish <rajoyish@gmail.com>
2024-05-31 10:26:57 +0545

    Add interactivity to mobile menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,23 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Slider
+const toggleOpen = document.getElementById("toggleOpen");
+const toggleClose = document.getElementById("toggleClose");
+const collapseMenu = document.getElementById("collapseMenu");
+
+function handleClick() {
+    if (collapseMenu.style.display === "block") {
+        collapseMenu.style.display = "none";
+    } else {
+        collapseMenu.style.display = "block";
+    }
+}
+
+toggleOpen.addEventListener("click", handleClick);
+toggleClose.addEventListener("click", handleClick);
