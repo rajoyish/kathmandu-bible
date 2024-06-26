01dcc458e59f8c86162368559d50325d496b54a5
rajoyish <rajoyish@gmail.com>
2024-06-26 13:07:50 +0545

    Clear git cache and apply .gitignore changes

---
+++ resources/views/about-us/message-from-the-president.blade.php
@@ -0,0 +1,66 @@
+<x-guest-layout>
+    <section class="isolate overflow-hidden bg-white px-6 md:px-8">
+        <div class="relative mx-auto max-w-2xl py-24 sm:py-32 md:max-w-4xl">
+            <x-page-heading class="lg:mb-32 mb-16">Message from the President</x-page-heading>
+            <figure class="grid grid-cols-1 items-center gap-x-6 gap-y-8 md:gap-x-10">
+                <div class="relative col-span-2 md:col-start-1 md:row-start-2">
+                    <svg class="absolute -top-12 left-0 -z-10 h-32 opacity-5" viewBox="0 0 24 24">
+                        <path
+                            d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+                    </svg>
+                    <blockquote class="text-xl font-semibold leading-8 text-slate-900 sm:text-2xl sm:leading-9">
+                        <p>KBI is committed to serving the Lord in partnership with local churches, training, and
+                            equipping individuals for God's mission in Nepal.</p>
+                    </blockquote>
+                </div>
+                <div class="col-end-1 w-16 md:row-span-4 md:w-72">
+                    <img class="rounded-xl bg-indigo-50 md:rounded-3xl"
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=576&h=576&q=80"
+                        alt="" />
+                </div>
+                <figcaption class="text-base md:col-start-1 md:row-start-3">
+                    <div class="font-semibold text-slate-900">Rev. Devilal Shrestha (Dev)</div>
+                    <div class="mt-1 text-slate-500">President/Founder</div>
+                </figcaption>
+            </figure>
+
+            <article class="space-y-4 py-8 md:py-16">
+                <p>Welcome in the Blessed Name of Our Redeemer, Jesus Christ!</p>
+
+                <p>I extend my heartfelt congratulations to the students currently enrolled in various programs at the
+                    Kathmandu Bible Institute & Research Centre (KBI). I also express my sincere appreciation to the
+                    local churches for trusting KBI and sending their students (pastors and leaders) for study. Your
+                    trust and support in our institution are deeply valued!</p>
+                <p>KBI is committed to serving the Lord in partnership with local churches, training, and equipping
+                    individuals for God's mission in Nepal. In our country's current context, we recognize the
+                    significant responsibility we have to bridge the gap between the church and society and between the
+                    church and the government. Bridging these gaps allows the gospel to touch people's hearts.
+                    Therefore, KBI encourages men and women from local churches to receive training in biblical,
+                    theological, missiological, historical, and contextual studies to help fill these gaps and fulfil
+                    the Great Commission of our Lord</p>
+
+                <p>KBI aims to strengthen local churches academically and spiritually equip their leaders while they
+                    remain actively involved in their church's ministries. These trained leaders can then actively and
+                    faithfully disciple others in their churches, protect their congregations from false teachings, and
+                    lay a solid foundation in the Word of God. In this way, KBI contributes to building strong local
+                    churches in our country.</p>
+
+                <p>KBI also prioritizes the development of indigenous writings and research works on Nepali Christianity
+                    by current and future students of this institution. Given the current state of our country, there is
+                    a significant need for developing indigenous writing, and KBI is eager to contribute to this effort.
+                </p>
+
+                <p>With a well-developed curriculum and experienced professors, KBI warmly welcomes all those who wish
+                    to be academically and spiritually trained in the Word of God to fulfil the Great Commission of
+                    Jesus Christ. We invite you to join our program!</p>
+
+                <p>Blessings,</p>
+                <p>Sign</p>
+                <p>
+                    <span>Rev. Devilal Shrestha (Dev)</span> <br />
+                    <span class="mt-1 text-slate-500">President/Founder</span>
+                </p>
+            </article>
+        </div>
+    </section>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home.index');
 
 Route::controller(AboutUsController::class)->group(function () {
-    Route::get('introduction', 'introduction')->name('introduction');
-    Route::get('vision-purpose-and-mission', 'visionPurposeAndMission')->name('vision-purpose-and-mission');
-    Route::get('core-values', 'CoreValues')->name('core-values');
-    Route::get('statement-of-faith', 'statementOfFaith')->name('statement-of-faith');
+  Route::get('introduction', 'introduction')->name('introduction');
+  Route::get('vision-purpose-and-mission', 'visionPurposeAndMission')->name('vision-purpose-and-mission');
+  Route::get('core-values', 'CoreValues')->name('core-values');
+  Route::get('statement-of-faith', 'statementOfFaith')->name('statement-of-faith');
+  Route::get('message-from-the-president', 'messageFromPresident')->name('message-from-president');
 });
 
 Route::controller(AcademicProgramController::class)->group(function () {
-    Route::get('academic-programs/certificate-in-theology-c-th', 'certificateInTheology')
-        ->name('certificate-in-theology-c-th');
-    Route::get('academic-programs/diploma-in-theology-dip-th', 'diplomaInTheology')
-        ->name('diploma-in-theology-dip-th');
-    Route::get('academic-programs/bachelor-of-theology-b-th', 'bachelorOfTheology')
-        ->name('bachelor-of-theology-b-th');
-    Route::get('academic-programs/m-a-in-christian-missions', 'mAInChristianMissions')
-        ->name('m-a-in-christian-missions');
+  Route::get('academic-programs/certificate-in-theology-c-th', 'certificateInTheology')
+    ->name('certificate-in-theology-c-th');
+  Route::get('academic-programs/diploma-in-theology-dip-th', 'diplomaInTheology')
+    ->name('diploma-in-theology-dip-th');
+  Route::get('academic-programs/bachelor-of-theology-b-th', 'bachelorOfTheology')
+    ->name('bachelor-of-theology-b-th');
+  Route::get('academic-programs/m-a-in-christian-missions', 'mAInChristianMissions')
+    ->name('m-a-in-christian-missions');
 });
 
 Route::get('advisors', AdvisorController::class)->name('advisors');
