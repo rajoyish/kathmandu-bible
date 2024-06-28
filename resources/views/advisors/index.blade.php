197ffb95224f6b0d7af2fe4085d7f8f96edd865d
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-28 22:24:19 +0545

    Refactor Advisors

--- resources/views/advisors/index.blade.php
+++ resources/views/advisors/index.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-  <section class="isolate overflow-hidden bg-blue-50 px-6 md:px-8">
+  <section class="isolate overflow-hidden bg-blue-100 px-6 md:px-8">
     <div class="relative mx-auto max-w-2xl py-24 sm:py-32 md:max-w-4xl">
       <x-top-mesh-gradient/>
       <x-page-heading>Board of Advisors</x-page-heading>
@@ -12,9 +12,9 @@
             <li>
               <img class="aspect-[14/16] w-full rounded-xl object-cover"
                    src="{{ url('storage/' . $advisor->photo) }}" alt="Photo of {{ $advisor->name }}"/>
-              <h3 class="mt-6 text-xl font-semibold leading-8 text-slate-600">{{ $advisor->name }}</h3>
-              <p class="leading-7 text-slate-500">{{ $advisor->designation }}</p>
-              <p class="text-sm leading-6 text-slate-500">{{ $advisor->organization }}</p>
+              <h3 class="mt-6 text-xl font-semibold leading-8 text-slate-800">{{ $advisor->name }}</h3>
+              <p class="leading-7 text-slate-700">{{ $advisor->designation }}</p>
+              <p class="text-sm leading-6 text-slate-700">{{ $advisor->organization }}</p>
             </li>
           @endforeach
           <!-- Advisor -->
