c4d92e9c4c9347c432e26611db67a6a070558171
rajoyish <rajoyish@gmail.com>
2024-07-12 17:53:14 +0545

    Add ShareThis

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,8 +20,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Reddit+Sans:ital,wght@0,200..900;1,200..900&display=swap"
         rel="stylesheet">
 
+    {{-- ShareThis --}}
+
+    <script type='text/javascript'
+        src='https://platform-api.sharethis.com/js/sharethis.js#property=66911a757f42390019536c40&product=sticky-share-buttons'
+        async='async'></script>
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/embla-slider.js', 'resources/js/nav.js', 'resources/js/photoswipe.js', 'resources/js/gallery-carousel.js'])
+
 </head>
 
 <body class="font-sans min-h-screen text-lg antialiased">
